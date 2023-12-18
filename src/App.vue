ec17d52f6c8325a12bbc5bc856250789c904c700
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2023-12-18 21:40:57 +0000

    Add transition to sidebar

--- src/App.vue
+++ src/App.vue
@@ -13,7 +13,19 @@ function toggleSideBar() {
 <template>
 	<NavBar @toggle-side-bar="toggleSideBar" />
 	<BlurLayer v-if="isSideBarOpen" />
-	<SideBar @toggle-side-bar="toggleSideBar" v-if="isSideBarOpen" />
+	<Transition>
+		<SideBar @toggle-side-bar="toggleSideBar" v-show="isSideBarOpen" />
+	</Transition>
 </template>
 
-<style scoped></style>
+<style scoped>
+.v-enter-active,
+.v-leave-active {
+	transition: transform 0.2s ease-out;
+}
+
+.v-enter-from,
+.v-leave-to {
+	transform: translateX(-100%);
+}
+</style>
