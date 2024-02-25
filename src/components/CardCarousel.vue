a610e43b7f4e465e2be1b773a7e0e3fcaa2a01e5
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-02-25 17:49:23 +0000

    Refactor cards into own component

--- src/components/CardCarousel.vue
+++ src/components/CardCarousel.vue
@@ -1,102 +1,74 @@
 <template>
   <div class="card-carousel-wrapper">
-    <div
-      class="card-carousel--nav__left"
-      :disabled="atHeadOfList"
-      @click="moveCarousel(-1)"
-    ></div>
+    <div class="card-carousel--nav__left" @click="goPrevious()"></div>
     <div class="card-carousel">
-      <div class="card-carousel--overflow-container">
-        <div
-          class="card-carousel-cards"
-          :style="{ transform: 'translateX(' + currentOffset + 'px)' }"
-        >
-          <div v-for="item in items" :key="item.id" class="card-carousel--card">
-            <img :src="item.image" />
-            <div class="card-carousel--card--footer">
-              <p>{{ item.name }}</p>
-              <p
-                v-for="(tag, index) in item.tag"
-                :key="index"
-                class="tag"
-                :class="index > 0 ? 'secondary' : ''"
-              >
-                {{ tag }}
-              </p>
-            </div>
-          </div>
-        </div>
+      <div class="card-carousel-cards">
+        <FeedbackCard
+          v-for="item in filteredItems"
+          :key="item.id"
+          :item="item"
+        ></FeedbackCard>
       </div>
     </div>
-    <div
-      class="card-carousel--nav__right"
-      :disabled="atEndOfList"
-      @click="moveCarousel(1)"
-    ></div>
+    <div class="card-carousel--nav__right" @click="goNext()"></div>
   </div>
 </template>
 
 <script setup>
-import { computed } from "vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
+import FeedbackCard from "src/components/FeedbackCard.vue";
 
-const currentOffset = ref(0);
-const windowSize = 3;
-const paginationFactor = 220;
+const cards_pos = ref(0);
+
+const filteredItems = computed(() => {
+  return [...items, ...items].slice(cards_pos.value, cards_pos.value + 3);
+});
+
+function goNext() {
+  cards_pos.value = (cards_pos.value + 1) % items.length;
+}
+
+function goPrevious() {
+  cards_pos.value = (cards_pos.value + items.length - 1) % items.length;
+}
+
 const items = [
   {
     name: "Kin Khao",
     image: "https://source.unsplash.com/200x200?girl",
-    tag: ["Thai"],
+    id: 0,
   },
   {
     name: "Jū-Ni",
     image: "https://source.unsplash.com/200x200?natural",
-    tag: ["Sushi", "Japanese", "$$$$"],
+    id: 1,
   },
   {
     name: "Delfina",
     image: "https://source.unsplash.com/200x200?beautiful",
-    tag: ["Pizza", "Casual"],
+    id: 2,
   },
   {
     name: "San Tung",
     image: "https://source.unsplash.com/200x200?attraction",
-    tag: ["Chinese", "$$"],
+    id: 3,
   },
   {
     name: "Anchor Oyster Bar",
     image: "https://source.unsplash.com/200x200?rose",
-    tag: ["Seafood", "Cioppino"],
+    id: 4,
   },
   {
     name: "Locanda",
     image: "https://source.unsplash.com/200x200?beach",
-    tag: ["Italian"],
+    id: 5,
   },
   {
     name: "Garden Creamery",
     image: "https://source.unsplash.com/200x200?forest",
-    tag: ["Ice cream"],
+    id: 6,
   },
 ];
-
-const atEndOfList = computed(() => {
-  return (
-    currentOffset.value <= paginationFactor * -1 * (items.length - windowSize)
-  );
-});
-const atHeadOfList = computed(() => {
-  return currentOffset.value === 0;
-});
-
-function moveCarousel(direction) {
-  if (direction === 1 && !atEndOfList.value) {
-    currentOffset.value -= paginationFactor;
-  } else if (direction === -1 && !atHeadOfList.value) {
-    currentOffset.value += paginationFactor;
-  }
-}
 </script>
 
 <style lang="scss" scoped>
@@ -111,10 +83,6 @@ function moveCarousel(direction) {
 .card-carousel {
   display: flex;
   justify-content: center;
-  width: 640px;
-}
-.card-carousel--overflow-container {
-  overflow: hidden;
 }
 .card-carousel--nav__left,
 .card-carousel--nav__right {
@@ -152,85 +120,10 @@ function moveCarousel(direction) {
   transition: transform 150ms ease-out;
   transform: translatex(0px);
 }
-.card-carousel-cards .card-carousel--card {
-  margin: 0 10px;
-  cursor: pointer;
-  box-shadow:
-    0 4px 15px 0 rgba(40, 44, 53, 0.06),
-    0 2px 2px 0 rgba(40, 44, 53, 0.08);
-  background-color: #fff;
-  border-radius: 4px;
-  z-index: 3;
-  margin-bottom: 2px;
-}
 .card-carousel-cards .card-carousel--card:first-child {
   margin-left: 0;
 }
 .card-carousel-cards .card-carousel--card:last-child {
   margin-right: 0;
-}
-.card-carousel-cards .card-carousel--card img {
-  vertical-align: bottom;
-  border-top-left-radius: 4px;
-  border-top-right-radius: 4px;
-  transition: opacity 150ms linear;
-  user-select: none;
-}
-.card-carousel-cards .card-carousel--card img:hover {
-  opacity: 0.5;
-}
-.card-carousel-cards .card-carousel--card--footer {
-  border-top: 0;
-  padding: 7px 15px;
-}
-.card-carousel-cards .card-carousel--card--footer p {
-  padding: 3px 0;
-  margin: 0;
-  margin-bottom: 2px;
-  font-size: 19px;
-  font-weight: 500;
-  color: #2c3e50;
-  user-select: none;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag {
-  font-size: 11px;
-  font-weight: 300;
-  padding: 4px;
-  background: rgba(40, 44, 53, 0.06);
-  display: inline-block;
-  position: relative;
-  margin-left: 4px;
-  color: #666a73;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag:before {
-  content: "";
-  float: left;
-  position: absolute;
-  top: 0;
-  left: -12px;
-  width: 0;
-  height: 0;
-  border-color: transparent rgba(40, 44, 53, 0.06) transparent transparent;
-  border-style: solid;
-  border-width: 8px 12px 12px 0;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag.secondary {
-  margin-left: 0;
-  border-left: 1.45px dashed white;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag.secondary:before {
-  display: none !important;
-}
-.card-carousel-cards .card-carousel--card--footer p.tag:after {
-  content: "";
-  position: absolute;
-  top: 8px;
-  left: -3px;
-  float: left;
-  width: 4px;
-  height: 4px;
-  border-radius: 2px;
-  background: white;
-  box-shadow: 0px 0px 0px #004977;
 }
 </style>
---
+++ src/components/FeedbackCard.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="card-carousel--card">
+    <img :src="item.image" />
+    <div class="card-carousel--card--footer">
+      <p>{{ item.name }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  item: {
+    type: Object,
+    required: true,
+  },
+});
+</script>
+
+<style lang="scss" scoped>
+.card-carousel--card {
+  margin: 0 10px;
+  cursor: pointer;
+  box-shadow:
+    0 4px 15px 0 rgba(40, 44, 53, 0.06),
+    0 2px 2px 0 rgba(40, 44, 53, 0.08);
+  background-color: #fff;
+  border-radius: 4px;
+  z-index: 3;
+  margin-bottom: 2px;
+
+  img {
+    vertical-align: bottom;
+    border-top-left-radius: 4px;
+    border-top-right-radius: 4px;
+    transition: opacity 150ms linear;
+    user-select: none;
+
+    & :hover {
+      opacity: 0.5;
+    }
+  }
+}
+
+.card-carousel--card--footer {
+  border-top: 0;
+  padding: 7px 15px;
+}
+
+.card-carousel--card--footer p {
+  padding: 3px 0;
+  margin: 0;
+  margin-bottom: 2px;
+  font-size: 19px;
+  font-weight: 500;
+  color: #2c3e50;
+  user-select: none;
+}
+</style>
