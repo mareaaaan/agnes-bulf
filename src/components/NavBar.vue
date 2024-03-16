94c20275bb7742eda3c4e23d00a280cfceb19dfd
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-03-16 12:03:18 +0000

    Reafactor ScrollDown into separate container

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,9 @@
 <script setup>
-import { useScroll } from "@vueuse/core";
 import SlideFromTop from "src/components/transitions/SlideFromTop.vue";
 import { defineProps, ref } from "vue";
 import PageHeader from "./PageHeader.vue";
+import NavBarLinks from "./NavBarLinks.vue";
+import useScrollUp from "src/composables/scrollUp";
 
 const props = defineProps({
   pages: {
@@ -20,30 +21,7 @@ function toggleSideBar() {
   emit("toggleSideBar");
 }
 
-const isLinksVisible = ref(true);
-
-let lastScrollPosition = 0;
-
-useScroll(window, {
-  onScroll: (e) => {
-    onScroll(e);
-  },
-});
-
-function onScroll() {
-  const currentScrollPosition = window.scrollY;
-
-  if (currentScrollPosition < 0) {
-    return;
-  }
-  // Stop executing this function if the difference between
-  // current scroll position and last scroll position is less than the offset
-  if (Math.abs(currentScrollPosition - lastScrollPosition) < 100) {
-    return;
-  }
-  isLinksVisible.value = currentScrollPosition < lastScrollPosition;
-  lastScrollPosition = currentScrollPosition;
-}
+const { isScrollUp } = useScrollUp();
 </script>
 
 <template>
@@ -56,16 +34,7 @@ function onScroll() {
     </aside>
     <PageHeader @toggle-side-bar="toggleSideBar" />
     <SlideFromTop>
-      <!-- <div v-show="isLinksVisible" class="links-container">
-        <a
-          v-for="(page, index) in props.pages"
-          :key="index"
-          class="link"
-          :href="page.path"
-          >{{ page.name }}</a
-        >
-      </div> -->
-      <NavBarLinks v-show="isLinksVisible" :pages="props.pages" />
+      <NavBarLinks v-show="isScrollUp" :pages="props.pages" />
     </SlideFromTop>
   </nav>
   <Teleport to="body">
@@ -116,81 +85,8 @@ nav {
   }
 }
 
-.logo {
-  padding: 1.25rem;
-  color: $secondary-color;
-
-  @media screen and (min-width: $desktop-width) {
-    padding-top: 0;
-  }
-}
-
-.logo__title {
-  font-family: "Playfair Display", serif;
-  font-size: 2.5rem;
-  text-transform: uppercase;
-
-  @media screen and (max-width: $desktop-width) {
-    font-size: 1.5625rem;
-  }
-}
-
-.logo__subtitle {
-  position: relative;
-  left: 4rem;
-  padding-top: 0.625rem;
-  font-family: "Halimun", "Whisper", sans-serif;
-  font-size: 1.25rem;
-
-  @media screen and (max-width: $desktop-width) {
-    padding-top: 0.3125rem;
-    font-size: 0.625rem;
-  }
-}
-
-.links-container {
-  background-color: $primary-color;
-  display: flex;
-  justify-content: center;
-  position: relative;
-  z-index: 1;
-  @media screen and (max-width: $desktop-width) {
-    display: none;
-  }
-}
-
 a:link,
 a:visited {
   text-decoration: none;
-}
-
-.link {
-  font-weight: 500;
-  text-transform: uppercase;
-  font-size: 0.9375rem;
-  color: $secondary-color;
-  position: relative;
-  padding: 0.625rem;
-  margin-left: 1.25rem;
-  margin-right: 1.25rem;
-}
-
-.link::before {
-  content: "";
-  position: absolute;
-  width: 100%;
-  height: 2px;
-  border-radius: 5px;
-  background-color: $secondary-color;
-  bottom: 0;
-  left: 0;
-  transform-origin: right;
-  transform: scaleX(0);
-  transition: transform 0.3s ease-in-out;
-}
-
-.link:hover::before {
-  transform-origin: left;
-  transform: scaleX(1);
 }
 </style>
