5d6a59fd21022477f2c7da0b72ecd9ca2e98d05f
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-04-13 14:56:18 +0100

    Add table of contents to large screens

--- src/components/TableOfContents.vue
+++ src/components/TableOfContents.vue
@@ -1,12 +1,23 @@
 <template>
   <aside ref="tableOfContents" class="table-of-contents">
-    <div class="table-of-contents--hierarchy">
-      <h3>Pe această pagină</h3>
-      <ul>
-        <li v-for="(header, index) in props.headers" :key="header">
-          <a :href="`#${index}`">{{ header }}</a>
-        </li>
-      </ul>
+    <div class="hierarchy-container">
+      <h3 class="hieararchy-title">Pe această pagină</h3>
+      <div class="hierarchy">
+        <ul>
+          <li
+            v-for="(header, index) in props.headers"
+            :key="header"
+            class="header-link-container"
+          >
+            <a
+              class="header-link"
+              :class="{ active: index == currentSection }"
+              :href="`#${index}`"
+              >{{ header }}</a
+            >
+          </li>
+        </ul>
+      </div>
     </div>
   </aside>
 </template>
@@ -15,6 +26,7 @@
 import { ref, computed, onMounted } from "vue";
 
 const props = defineProps({
+  // eslint-disable-next-line vue/require-default-prop
   headers: Array,
 });
 
@@ -26,9 +38,28 @@ const topInPixels = computed(() => {
   return top.value + "px";
 });
 
+const currentSection = ref(0);
+
 onMounted(() => {
   top.value = tableOfContents.value.getBoundingClientRect().top;
-  console.log(top.value);
+
+  const observer = new IntersectionObserver(
+    (entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          currentSection.value = entry.target.getAttribute("id");
+          console.log(currentSection.value);
+        }
+      });
+    },
+    {
+      rootMargin: "0px 0px -90% 0px",
+    },
+  );
+
+  document.querySelectorAll(".section__title").forEach((section) => {
+    observer.observe(section);
+  });
 });
 </script>
 
@@ -41,14 +72,36 @@ onMounted(() => {
   box-sizing: border-box;
 }
 
-.table-of-contents--hierarchy {
+.hierarchy-container {
   position: sticky;
   top: v-bind(topInPixels);
 }
 
 // STYLES
 
-.table-of-contents--hierarchy {
-  padding: 1rem;
+.hierarchy-container {
+  text-align: start;
+}
+
+.hieararchy-title {
+  font-size: $font-size;
+  margin-bottom: 0.625rem;
+}
+
+.header-link-container {
+  font-size: $s-font-size;
+  border-left: 1px solid $secondary-color;
+  padding-left: 2em;
+}
+
+.header-link {
+  text-decoration: none;
+
+  color: $secondary-color;
+}
+
+.header-link.active {
+  font-weight: bold;
+  border-color: black;
 }
 </style>
