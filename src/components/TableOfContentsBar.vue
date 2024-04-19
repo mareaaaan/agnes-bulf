e227d1e438c46d5e57350df862123c241544b3b6
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-04-19 21:16:08 +0100

    Add TableOfContentsbar icon

--- src/components/TableOfContentsBar.vue
+++ src/components/TableOfContentsBar.vue
@@ -2,7 +2,20 @@
   <section ref="section" class="section max-width-container">
     <div class="section-grid max-width">
       <button class="expand-button" @click="toggleExpandHierarchy()">
-        Pe această pagină >
+        Pe această pagină
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+          focusable="false"
+          viewBox="0 0 24 24"
+          class="icon"
+          :class="{ expanded: isExpanded }"
+          data-v-e5e7bc23=""
+        >
+          <path
+            d="M9,19c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4l5.3-5.3L8.3,6.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l6,6c0.4,0.4,0.4,1,0,1.4l-6,6C9.5,18.9,9.3,19,9,19z"
+          ></path>
+        </svg>
       </button>
       <PageHierarchy v-if="isExpanded" :headers="props.headers" />
     </div>
@@ -75,6 +88,7 @@ function toggleExpandHierarchy() {
 
 .section {
   padding-inline: 1rem;
+  padding-block-end: 0.5rem;
   background-color: $light-color;
 }
 
@@ -91,5 +105,19 @@ button {
 .expand-button {
   text-align: right;
   font-size: $s-font-size;
+}
+
+.icon {
+  display: inline-block;
+  vertical-align: middle;
+  margin-left: 2px;
+  width: 14px;
+  height: 14px;
+  fill: $secondary-color;
+  transition: transform 0.3s;
+
+  &.expanded {
+    transform: rotate(90deg);
+  }
 }
 </style>
