7fd4bebaa9c8dbfda5f4cdb7aabb88d61799e3ff
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-04-20 16:30:52 +0100

    Refactor ExpandIcon into separate component

--- src/components/TableOfContentsBar.vue
+++ src/components/TableOfContentsBar.vue
@@ -4,19 +4,10 @@
       <div class="section-grid max-width">
         <button class="expand-button" @click="toggleExpandHierarchy()">
           Pe această pagină
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            aria-hidden="true"
-            focusable="false"
-            viewBox="0 0 24 24"
-            class="icon"
+          <ExpandIcon
+            class="expand-icon"
             :class="{ expanded: isExpanded }"
-            data-v-e5e7bc23=""
-          >
-            <path
-              d="M9,19c-0.3,0-0.5-0.1-0.7-0.3c-0.4-0.4-0.4-1,0-1.4l5.3-5.3L8.3,6.7c-0.4-0.4-0.4-1,0-1.4s1-0.4,1.4,0l6,6c0.4,0.4,0.4,1,0,1.4l-6,6C9.5,18.9,9.3,19,9,19z"
-            ></path>
-          </svg>
+          ></ExpandIcon>
         </button>
       </div>
     </section>
@@ -30,6 +21,7 @@
 import { useElementBounding } from "@vueuse/core";
 import { ref, computed } from "vue";
 import PageHierarchyBar from "./PageHierarchyBar.vue";
+import ExpandIcon from "./ExpandIcon.vue";
 
 const section = ref(null);
 
@@ -113,13 +105,7 @@ button {
   font-size: $s-font-size;
 }
 
-.icon {
-  display: inline-block;
-  vertical-align: middle;
-  margin-left: 2px;
-  width: 14px;
-  height: 14px;
-  fill: $secondary-color;
+.expand-icon {
   transition: transform 0.3s;
 
   &.expanded {
