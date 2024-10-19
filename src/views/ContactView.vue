ab4943589d830fc8c7f28a631a7745b44eff3412
Nicolae-Marian Gartu <gartu.marian1973@gmail.com>
2024-10-19 17:25:47 +0100

    Add contact page data

--- src/views/ContactView.vue
+++ src/views/ContactView.vue
@@ -1,7 +1,40 @@
 <template>
-  <div class="class"></div>
+  <PageTitle v-if="data" :data="data" />
+  <component
+    :is="getComponent(section._type)"
+    v-for="(section, index) in data?.content"
+    :key="index"
+    :data="section"
+  />
+  <WavyDivider :is-light-to-dark="false" />
+  <PageFooter class="light-background" />
 </template>
 
-<script setup></script>
+<script setup>
+import { fetchPageData } from "src/client";
+import { ref, onBeforeMount } from "vue";
+import ContainerizedCardSection from "src/components/sections/ContainerizedCardSection.vue";
+import ContainerizedTextImageSection from "src/components/sections/ContainerizedTextImageSection.vue";
+
+const data = ref(null);
+
+const getComponent = (sectionType) => {
+  const sectionComponentPairs = {
+    textWithIllustration: ContainerizedTextImageSection,
+    floatingText: ContainerizedCardSection,
+  };
+
+  return sectionComponentPairs[sectionType];
+};
+
+async function fetchData() {
+  var pageData = await fetchPageData("contact");
+  data.value = pageData;
+}
+
+onBeforeMount(() => {
+  fetchData();
+});
+</script>
 
 <style lang="scss" scoped></style>
